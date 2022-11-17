368948b71f3e9a2605a41b38e805b884789f95be
Leonardo Asad <leonardo.asad.89@gmail.com>
2022-11-17 19:27:30 +1300

    Get Cart Items By Cart Id changed implementation

--- queries/cartQueries.js
+++ queries/cartQueries.js
@@ -1,27 +1,64 @@
 const pool = require('./dbConfig').pool;
 
-const getProductsByCartId = (request, response, next) => {
+// const verifyCartId = (request, response, next, cartId) => {
+//   //const cartId = parseInt(cartId);
+
+//   pool.query("SELECT * FROM cart WHERE id = $1",
+//   [cartId],
+//   (error, results) => {
+//     if (error) {
+//       return response.status(500).send("Error During DB query: Verify Cart Id.");
+//     };
+//     if (results.rows.length === 0) {
+//       return response.send("Non-Existent Cart");
+//     };
+//     request.cartId = results.rows[0].id;
+//     next();
+//   });
+// };
+
+const getAllCartItems = (request, response, next) => {
+  if (request.query.hasOwnProperty('cart_id')) {
+    request.cartId = request.query.cart_id;
+    return next();
+  }
+
+  pool.query(
+    `
+    SELECT *
+    FROM carts_products;
+    `,
+    (error, results) => {
+      if (error) {
+        return response.status(500).send("Error During DB query: Get All Cart Items");
+      }
+      response.send(results.rows);
+    }
+  )
+}
+
+const getCartItemsByCartId = (request, response, next) => {
   const cartId = parseInt(request.cartId);
 
   pool.query(
   `
-  WITH previous_query AS (
-    SELECT product_id, SUM(quantity) as quantity_order
+    SELECT
+      carts_products.id,
+      name,
+      product.quantity as in_stock,
+      carts_products.quantity as quantity_order,
+      product.price as price,
+      product.price*carts_products.quantity as total_price
     FROM carts_products
-    GROUP BY product_id, cart_id
-    HAVING cart_id = $1
-  )
-  SELECT id, name, quantity as in_stock, quantity_order, price, quantity_order*price as total_price
-  FROM previous_query
-  JOIN product
-    ON previous_query.product_id = product.id
-  WHERE active = TRUE;
+    JOIN product
+      ON carts_products.product_id = product.id
+    WHERE cart_id = $1;
   `,
   [cartId],
   (error, results) => {
     if (error) {
       return response.status(500).send("Error During DB query: Get Products By Cart Id");
-    }
+    };
     if (results.rows.length === 0) {
       return response.send("Empty Cart");
     };
@@ -30,124 +67,133 @@ const getProductsByCartId = (request, response, next) => {
   });
 };
 
-const emptyCart = (request, response, next) => {
-  const cartId = parseInt(request.cartId);
+// const emptyCart = (request, response, next) => {
+//   const cartId = parseInt(request.cartId);
 
-  pool.query(
-    `
-    DELETE FROM carts_products
-    WHERE cart_id = $1;
-    `,
-    [cartId],
-    (error, results) => {
-      if (error) {
-        return response.status(500).send("Error During DB query: Empty Cart.");
-      };
-      console.log("Cart Emptied Successfully");
-      next();
-    }
-  );
-};
+//   pool.query(
+//     `
+//     DELETE FROM carts_products
+//     WHERE cart_id = $1;
+//     `,
+//     [cartId],
+//     (error, results) => {
+//       if (error) {
+//         return response.status(500).send("Error During DB query: Empty Cart.");
+//       };
+//       console.log("Cart Emptied Successfully");
+//       next();
+//     }
+//   );
+// };
 
-const getCartPrice = (request, response, next) => {
-  const cartId = parseInt(request.cartId);
+// const getCartPrice = (request, response, next) => {
+//   const cartId = parseInt(request.cartId);
 
-  pool.query(
-    `
-    SELECT SUM(price) as total_price
-    FROM carts_products
-    JOIN product
-      ON carts_products.product_id = product.id
-    WHERE cart_id = $1 AND active = TRUE;
-    `,
-    [cartId],
-    (error, results) => {
-      if (error) {
-        return response.status(500).send("Error During DB query: Get Cart Price.");
-      }
-      request.totalPay = results.rows[0].total_price;
-      next();
-    });
-};
+//   pool.query(
+//     `
+//     SELECT SUM(price) as total_price
+//     FROM carts_products
+//     JOIN product
+//       ON carts_products.product_id = product.id
+//     WHERE cart_id = $1 AND active = TRUE;
+//     `,
+//     [cartId],
+//     (error, results) => {
+//       if (error) {
+//         return response.status(500).send("Error During DB query: Get Cart Price.");
+//       };
+//       request.totalPay = results.rows[0].total_price;
+//       next();
+//     });
+// };
 
-const createCart = (request, response, next) => {
-  const userId = parseInt(request.user.id);
+// const createCart = (request, response, next) => {
+//   const userId = parseInt(request.user.id);
 
-  pool.query("INSERT INTO cart (user_id) VALUES ($1) RETURNING *",
-  [userId],
-  (error, results) => {
-    if (error) {return next(error);}
-    response.status(201).send(`Created new Cart with Id: ${results.rows[0].id}`);
-  });
-};
+//   pool.query("INSERT INTO cart (user_id) VALUES ($1) RETURNING *",
+//   [userId],
+//   (error, results) => {
+//     if (error) {return next(error);}
+//     response.status(201).send(`Created new Cart with Id: ${results.rows[0].id}`);
+//   });
+// };
 
-const addProductToCart = (request, response, next) => {
-  const cartId = parseInt(request.cartId);
-  const { productId, quantity } = request.body;
+// const addProductToCart = (request, response, next) => {
+//   const cartId = parseInt(request.cartId);
+//   const { productId, quantity } = request.body;
 
-  pool.query("INSERT INTO carts_products (cart_id, product_id, quantity) VALUES ($1, $2, $3) RETURNING *",
-  [cartId, productId, quantity],
-  (error, results) => {
-    if (error) {return next(error);}
-    response.status(201).send(`Added Product Id: ${productId} to cart Id: ${cartId}`);
-  });
-};
+//   pool.query("INSERT INTO carts_products (cart_id, product_id, quantity) VALUES ($1, $2, $3) RETURNING *",
+//   [cartId, productId, quantity],
+//   (error, results) => {
+//     if (error) {return next(error);}
+//     response.status(201).send(`Added Product Id: ${productId} to cart Id: ${cartId}`);
+//   });
+// };
 
-const verifyCartId = (request, response, next, id) => {
-  const cartId = parseInt(id);
+// const verifyStock = (request, response, next) => {
+//   const cartId = parseInt(request.cartId);
 
-  pool.query("SELECT * FROM cart WHERE id = $1",
-  [cartId],
-  (error, results) => {
-    if (error) {
-      return response.status(500).send("Error During DB query: Verify Cart Id.");
-    }
-    if (results.rows.length === 0) {
-      return response.send("Non-Existent Cart");
-    }
-    request.cartId = results.rows[0].id;
-    next();
-  });
-};
+//   pool.query(
+//     `
+//     WITH previous_query AS (
+//       SELECT product_id, SUM(quantity) as quantity_order
+//       FROM carts_products
+//       GROUP BY product_id, cart_id
+//       HAVING cart_id = $1
+//     )
+//     SELECT id, name, quantity as in_stock, quantity_order
+//     FROM previous_query
+//     JOIN product
+//       ON previous_query.product_id = product.id
+//     WHERE active = TRUE AND quantity_order > quantity;
+//     `,
+//     [cartId],
+//     (error, results) => {
+//       if (error) {
+//         return response.status(500).send("Error During DB query: Verify Stock.");
+//       };
+//       if (results.rows.length > 0) {
+//         return response.json({
+//           message: "The following products are Out of Stock, please update your order",
+//           products: results.rows
+//         })};
+//       next();
+//     }
+//   )
+// }
 
-const verifyStock = (request, response, next) => {
-  const cartId = parseInt(request.cartId);
+// const updateCartItem = (request, response, next) => {
+//   const itemId = parseInt(request.params.itemId);
+//   const { quantity } = request.body;
 
-  pool.query(
-    `
-    WITH previous_query AS (
-      SELECT product_id, SUM(quantity) as quantity_order
-      FROM carts_products
-      GROUP BY product_id, cart_id
-      HAVING cart_id = $1
-    )
-    SELECT id, name, quantity as in_stock, quantity_order
-    FROM previous_query
-    JOIN product
-      ON previous_query.product_id = product.id
-    WHERE active = TRUE AND quantity_order > quantity;
-    `,
-    [cartId],
-    (error, results) => {
-      if (error) {
-        return response.status(500).send("Error During DB query: Verify Stock.");
-      }
-      if (results.rows.length > 0) {
-        return response.json({
-          message: "The following products are Out of Stock, please update your order",
-          products: results.rows
-        })};
-      next();
-    }
-  )
-}
+//   pool.query(
+//     `
+//     UPDATE carts_products
+//     SET quantity = $1
+//     WHERE id = $2
+//     RETURNING *
+//     `,
+//     [quantity, itemId],
+//     (error, results) => {
+//       if (error) {return next(error);}
+//       return response.send(
+//         `Order Updated successfilly.
+//         Cart Id: ${results.rows[0].cart_id}.
+//         Product Id: ${results.rows[0].product_id}.
+//         New quantity: ${results.rows[0].quantity}
+//         `
+//       )
+//     });
+// };
 
 module.exports = {
-  getProductsByCartId,
-  createCart,
-  addProductToCart,
-  verifyCartId,
-  getCartPrice,
-  verifyStock,
-  emptyCart,
+  getAllCartItems,
+  getCartItemsByCartId,
+  // createCart,
+  // addProductToCart,
+  // //verifyCartId,
+  // getCartPrice,
+  // verifyStock,
+  // emptyCart,
+  // updateCartItem,
 };
--- routes/cart.js
+++ routes/cart.js
@@ -1,33 +1,35 @@
 const cartRouter = require('express').Router();
 const dbCart = require('../queries/cartQueries');
-const dbOrder = require('../queries/order');
-const dbProduct = require('../queries/productQueries');
-const userPermissions = require('../permissions/userPermissions');
+// const dbOrder = require('../queries/orderQueries');
+// const dbProduct = require('../queries/productQueries');
+// const userPermissions = require('../permissions/userPermissions');
 
-cartRouter.param('id', dbCart.verifyCartId);
+//cartRouter.param('cartId', dbCart.verifyCartId);
 
-cartRouter.get('/:id', dbCart.getProductsByCartId, (request, response) => {
+cartRouter.get('/', dbCart.getAllCartItems , dbCart.getCartItemsByCartId, (request, response) => {
   response.status(200).json(request.products);
 });
 
-cartRouter.post('/', userPermissions.isLoggedIn, dbCart.createCart);
+// cartRouter.put('/:itemId', dbCart.updateCartItem);
 
-cartRouter.post('/:id', userPermissions.isLoggedIn, dbCart.addProductToCart);
+// cartRouter.post('/', userPermissions.isLoggedIn, dbCart.createCart);
 
-cartRouter.post('/:id/checkout',
-  userPermissions.isLoggedIn,
-  dbCart.verifyStock,
-  dbCart.getProductsByCartId,
-  dbCart.getCartPrice,
-  dbOrder.createOrder,
-  dbCart.emptyCart,
-  dbProduct.updateProductStock,
-  (request, response) => {
-    response.json({
-      message: "Operation executed successfully!",
-      purchases: request.products,
-      totalDue: request.totalPay
-    });
-  });
+// cartRouter.post('/:cartId', userPermissions.isLoggedIn, dbCart.addProductToCart);
+
+// cartRouter.post('/:id/checkout',
+//   userPermissions.isLoggedIn,
+//   dbCart.verifyStock,
+//   dbCart.getProductsByCartId,
+//   dbCart.getCartPrice,
+//   dbOrder.createOrder,
+//   dbCart.emptyCart,
+//   dbProduct.updateProductStock,
+//   (request, response) => {
+//     response.json({
+//       message: "Operation executed successfully!",
+//       purchases: request.products,
+//       totalDue: request.totalPay
+//     });
+//   });
 
 module.exports = cartRouter;
