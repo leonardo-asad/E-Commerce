c1e3a154b2c8286128ddca920333f32aaa52ba42
Leonardo Asad <leonardo.asad.89@gmail.com>
2022-12-26 12:33:18 +1300

    Change Backend URL Env variable

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,15 +1,14 @@
 const passport = require('passport');
+const db = require('../db/userQueries')
 const LocalStrategy = require('passport-local').Strategy;
 var GoogleStrategy = require('passport-google-oauth20');
-const bcrypt = require('bcrypt');
-const db = require('../db/userQueries')
 
 passport.use(new LocalStrategy(db.verifyUser));
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.BACKEND_URL}/api/users/oauth2/redirect/google`,
+  callbackURL: `${process.env.BACKEND_URL}/users/oauth2/redirect/google`,
   scope: [ 'profile' ],
   state: true,
   proxy: true,
