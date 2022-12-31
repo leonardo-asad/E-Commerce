1b95853959b296b5cb425b17835eeb49d5798ac7
Leonardo Asad <leonardo.asad.89@gmail.com>
2022-12-31 14:38:29 +1300

    Implement Payment Status Messages

--- client/src/components/CheckoutForm/CheckoutForm.tsx
+++ client/src/components/CheckoutForm/CheckoutForm.tsx
@@ -48,6 +48,8 @@ export default function CheckoutForm() {
     }
 
     setIsLoading(true);
+    setSuccessMessage(undefined);
+    setError(undefined);
 
     const result = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
